9211817a7badcacb778151f030e2c8c086643b29
williamsh.huang <tisamwilliam@gmail.com>
2022-06-29 10:07:13 +0800

    first commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "google" {
+  project = "openshift-build-354002"
+  region  = "asia-east1"
+  zone    = "asia-east1-c"
+}
+
+resource "google_compute_instance" "default" {
+  count        = 2
+
+  name         = "terraform-demo-${count.index}"
+  machine_type = "e2-micro"
+  zone         = "asia-east2-a"
+
+  network_interface {
+    network = "default"
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-stream-8"
+    }
+  }
+}
