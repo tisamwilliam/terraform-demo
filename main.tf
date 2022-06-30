2c44157dd14304aff5abf47fe52bbfb330fd701d
William Huang <tisamwilliam@gmail.com>
2022-06-30 15:59:03 +0800

    add: create 2 gcp instance

--- main.tf
+++ main.tf
@@ -2,33 +2,28 @@ provider "google" {
   project = "openshift-build-354002"
 }
 
-data "google_compute_image" "centos_image" {
-  family  = "centos-stream-8"
-  project = "centos-cloud"
-}
-
-resource "google_compute_address" "static" {
-  name = "ipv4-address"
-  region  = "asia-east1"
+data "tfe_outputs" "foo" {
+    organization = "william-test"
+      workspace = "Create-GCP-Instance"
 }
 
 resource "google_compute_instance" "default" {
-  count        = 0
+  count        = 2
 
-  name         = "terraform-version-control-${count.index}"
+  name         = "terraform-test-${count.index}"
   machine_type = "e2-micro"
   zone         = "asia-east1-a"
 
   network_interface {
     network = "default"
     access_config {
-      nat_ip = google_compute_address.static.address
     }
   }
 
   boot_disk {
     initialize_params {
-      image = data.google_compute_image.centos_image.self_link
+  
+      image = "centos-cloud/centos-stream-8"
     }
   }
 }
